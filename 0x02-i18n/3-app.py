f9146beb6d36c1435ef70a95db35d456ebcacbf0
zibxto <christianaziba@gmail.com>
2024-04-18 20:34:46 +0100

    solve task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Module"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """set config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=False)
+def home() -> str:
+    """ display a HTML page"""
+    return render_template('1-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Get the best matching language."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
