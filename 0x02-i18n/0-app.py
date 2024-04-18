b37e0c2a96a791d8fd9ec53afd07d0268e686010
zibxto <christianaziba@gmail.com>
2024-04-18 13:15:41 +0100

    update file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
 from flask import Flask, render_template
-app: Flask = Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def home() -> str:
+def index() -> str:
     """ display a HTML page"""
     return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
